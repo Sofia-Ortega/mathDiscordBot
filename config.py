22ac14012dc2296b2dd3eeb42048c59c9daaefad
andrew-fennell <andrew.fennell1999@me.com>
2020-11-07 12:56:55 -0600

    Changed command structure.

--- config.py
+++
@@ -1,6 +0,0 @@
-
-CONFIG = {
-    'token': 'NzcyOTk5MTgxNjI2NTA3MjY0.X6C1lg.lyAHvbbKhkjF1jpEE_Eh7Ydh1xY', # add your token here
-    'channel_id': 758164860252061729, # channel id
-    'bot_prefix': '--' # customize command prefix
-}
