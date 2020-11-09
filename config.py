1ce1a933f158b899172c85fbaa0dd76c6a3045a9
andrew-fennell <andrew.fennell1999@me.com>
2020-11-09 13:13:03 -0600

    added message on join, changed embed creation

--- config.py
+++
@@ -1,9 +0,0 @@
-# MUST DEFINE: token, channel_id, bot_prefix
-# OPTIONAL: category
-
-CONFIG = {     
-    'token': '', # add your token here
-    'channel_id': , # main/general channel (This MUST be an INT, not a string)
-    'category': '', # if you want the game channels to be created in a particular category
-    'bot_prefix': '!' # command prefix (i.e. !start or -start)
-}
